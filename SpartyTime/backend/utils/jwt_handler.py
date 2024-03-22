1763f1134bbf832533bd2012e36d1192c22ea1fb
Sahran <82019509+SockYeh@users.noreply.github.com>
2024-03-22 18:41:55 +0530

    Update code to use Pydanctic.

--- SpartyTime/backend/utils/jwt_handler.py
+++ SpartyTime/backend/utils/jwt_handler.py
@@ -2,9 +2,32 @@ import time
 
 import jwt
 from decouple import config
+import pydantic
+from bson import ObjectId
+from bson.errors import InvalidId
 
 JWT_SECRET = config("secret")
 JWT_ALGORITHM = config("algorithm")
+
+
+class JWTExpired(Exception):
+    def __init__(self) -> None:
+        super().__init__("JWT token has expired. ")
+
+
+class JWTToken(pydantic.BaseModel):
+    user_id: str
+    expires: float = time.time() + 60 * 60 * 24 * 7
+
+    @pydantic.validator("user_id", pre=True, always=True)
+    def check_user_id(cls, value):
+        if not ObjectId.is_valid(value):
+            raise InvalidId(f"{value} is not a valid ObjectId. ")
+
+    @pydantic.validator("expires", pre=True, always=True)
+    def check_expiry(cls, value):
+        if value < time.time():
+            raise JWTExpired()
 
 
 def token_response(token: str) -> dict:
@@ -12,20 +35,13 @@ def token_response(token: str) -> dict:
 
 
 def signJWT(user_id: str) -> dict:
-    expiry = time.time() + 60 * 60 * 24 * 7
-    payload = {"user_id": user_id, "expires": expiry}
+    payload: JWTToken = JWTToken(user_id=user_id)
     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
     return token_response(token)
 
 
 def decode_jwt(token: str) -> dict:
-    try:
-        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-        decoded_token["status"] = "Success"
-        return (
-            decoded_token
-            if decoded_token["expires"] >= time.time()
-            else {"status": "Expired"}
-        )
-    except Exception as e:
-        return {"status": "Failed", "message": str(e)}
+    decoded_token: JWTToken = JWTToken(
+        **jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+    )
+    return decoded_token
